9d85dc369d3b5e26146a0a4419baf1b15255551f
Gaia <gaia.falcone@itsrizzoli.it>
2025-04-29 16:43:19 +0200

    ho iniziato il progetto

---
+++ assets/scripts/app.js
@@ -0,0 +1,62 @@
+const form = document.querySelector("#requests form");
+const input = document.querySelector("#requests input");
+const msg = document.querySelector("#requests .msg");
+const cities = document.getElementById("results");
+const apiKey = "9e5a252b5eeca9d81166f9b9cae3294c";
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+
+    let city = input.value;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            let name = data.name;
+            let country = data.sys.country;
+            let temp = data.main.temp;
+            let icon = data.weather[0].icon;
+            let description = data.weather[0].description;
+
+            console.log(name, country, temp);
+
+            let card = document.createElement("div");
+            card.classList.add("card-meteo");
+
+            let templateCard = `
+                <div class="card-meteo-header">
+                    <h2 class="card-meteo-title">
+                        <span>${name}</span>
+                        <sup>${country}</sup>
+                    </h2>
+                   </div>
+                   <div class="card-meteo-body">
+                        <div class="card-meteo-temp">
+                            <span>${temp}</span>
+                            <sup>°C</sup>
+                        </div>
+                        <figure class="card-meteo-figure">
+                            <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="">
+                            <figcaption>${description}</figcaption>
+                        </figure>   
+                       </div>   
+                       
+        `;
+
+        card.innerHTML = templateCard;
+        cities.appendChild(card);
+    })
+
+    .catch(() => {
+        msg.textContent = "Città non trovata 🫠, cerca una città diversa"
+    });
+
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+
+
+});
